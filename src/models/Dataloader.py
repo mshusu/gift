ccaa309c3dea04c209d854cf3abc149b8b7a0161
Huifeng Sun <sunhuifeng@bza.edu.cn>
2026-07-14 22:56:20 +0800

    restore --legacy_aux_neg_sampler

--- src/models/Dataloader.py
+++ src/models/Dataloader.py
@@ -101,6 +101,8 @@ class Dataset(BaseDataset):
         }
 
     def _use_legacy_aux_neg_sampling(self):
+	return getattr(self.args, 'legacy_aux_neg_sampler') 
+
         legacy_flag = getattr(self.args, 'legacy_aux_neg_sampler', -1)
         legacy_models = {'PISA_LGN', 'Contrastive_LGN'}
         if legacy_flag < 0:
